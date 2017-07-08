855d1b7850f11a599c4e2802c67e8dffab749c71
github username <krishel.lasam@gmail.com>
2017-07-08 15:54:41 -0400

    add react-router-dom to app.js

--- src/App.js
+++ src/App.js
@@ -1,21 +1,65 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
+import {
+  BrowserRouter as Router,
+  Route
+} from 'react-router-dom'
+import NavBar from './components/NavBar.js';
+import PropTypes from 'prop-types';
+
+const Compare = () => (
+  <div>
+    <h2>Compare</h2>
+  </div>
+)
+
+const ProductsPage = () => (
+  <div>
+    <h2>ProductsPage</h2>
+  </div>
+)
+
+const IngredientsPage = () => (
+  <div>
+    <h2>IngredientsPage</h2>
+  </div>
+)
+
+const Product = ({ match }) => (
+  <div>
+    <h3>{match.params.productId}</h3>
+  </div>
+)
+
+const ReactRouter = () => (
+  <Router>
+    <div className="navbar">
+      < NavBar />
+      <hr/>
+
+      <Route exact path="/" component={Compare}/>
+      <Route path="/products" component={ProductsPage}/>
+      <Route path="/ingredients" component={IngredientsPage}/>
+    </div>
+  </Router>
+)
 
 class App extends Component {
   render() {
     return (
       <div className="App">
+        <h1 className="text">Luminance</h1>
         <div className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h2>Welcome to React</h2>
+          < ReactRouter />
         </div>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+        {this.props.children}
       </div>
     );
   }
 }
+
+App.propTypes = {  
+  children: PropTypes.object.isRequired
+};
 
 export default App;
