d9157d3b7ee9ef06059b343fb095e638f1a100ff
github username <krishel.lasam@gmail.com>
2017-07-14 06:11:39 -0400

    add redux-devtools-extension to store

--- src/store/configureStore.js
+++ src/store/configureStore.js
@@ -1,10 +1,11 @@
 import {createStore, applyMiddleware} from 'redux';  
 import rootReducer from '../reducers/rootReducer';  
 import thunk from 'redux-thunk';
+import { composeWithDevTools } from 'redux-devtools-extension'
 
 export default function configureStore() {  
   return createStore(
-    rootReducer,
-    applyMiddleware(thunk)
+    rootReducer, composeWithDevTools(
+    applyMiddleware(thunk))
   );
 }
