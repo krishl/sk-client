a7b5763dc07ca7cc824203126a0ff3d2141164f4
github username <krishel.lasam@gmail.com>
2017-07-14 06:08:38 -0400

    add createProduct to productApi.js

--- src/api/productApi.js
+++ src/api/productApi.js
@@ -6,4 +6,21 @@ export default class ProductApi {
       return error;
     });
   }
+
+  static createProduct(product) {
+    const request = new Request('http://localhost:3001/api/v1/products/', {
+      method: 'POST',
+      headers: new Headers({
+        'Content-Type': 'application/json'
+      }), 
+      body: JSON.stringify({product: product})
+    });
+
+
+    return fetch(request).then(response => {
+      return response.json();
+    }).catch(error => {
+      return error;
+    });
+  }
 }
