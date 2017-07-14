d49eb7d6f677d5a004eb6d544dfd5a53a42974c2
github username <krishel.lasam@gmail.com>
2017-07-14 06:15:01 -0400

    create initial ProductForm without ingredients

---
+++ src/components/ProductForm.js
@@ -0,0 +1,49 @@
+import React, {Component} from 'react';  
+import PropTypes from 'prop-types'; 
+import TextField from './TextField';  
+import {StyleSheet, css} from 'aphrodite-jss';
+
+const sheet = StyleSheet.create({
+  content: {
+    width: '69%',
+    height: '100%',
+    float: 'right',
+    position: 'relative'
+  },
+})
+
+class ProductForm extends Component {  
+  render() {
+    return (
+      <div className={css(sheet.content)}>
+        <h2>New Product</h2>
+        <form>
+          <TextField
+            name="name"
+            label="Name"
+            value={this.props.product.name}
+            onChange={this.props.onChange}/>
+          <br />
+          <TextField
+            name="brand"
+            label="Brand"
+            value={this.props.product.brand}
+            onChange={this.props.onChange}/>
+
+          <input
+            type="submit"
+            onClick={this.props.onSubmit}/>
+        </form>
+      </div>
+  );
+  }
+}
+
+ProductForm.propTypes = {  
+  product: PropTypes.object.isRequired,
+ // ingredients: PropTypes.array.isRequired,
+  onChange: PropTypes.func.isRequired,
+  onSubmit: PropTypes.func.isRequired
+};
+
+export default ProductForm;  
