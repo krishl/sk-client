338448a479ed4703e9447467dadf292e15da493e
github username <krishel.lasam@gmail.com>
2017-07-14 06:11:11 -0400

    add CREATE_PRODUCT_SUCCESS to productReducer.js

--- src/reducers/productReducer.js
+++ src/reducers/productReducer.js
@@ -1,10 +1,15 @@
 import initialState from './initialState';
-import * as types from '../actions/actionTypes';  
+import * as types from '../actions/actionTypes'; 
 
 export default function productReducer(state = initialState.products, action) {  
   switch(action.type) {
     case types.LOAD_PRODUCTS_SUCCESS:
       return action.products
+    case types.CREATE_PRODUCT_SUCCESS:
+      return [
+        ...state,
+        action.product
+      ]
     default: 
       return state;
   }
