60d25d51f625ee010f25030ada736aa103fae61d
github username <krishel.lasam@gmail.com>
2017-07-15 04:22:19 -0400

    remove ingredientApi.js

--- src/api/ingredientApi.js
+++
@@ -1,9 +0,0 @@
-export default class IngredientApi {  
-  static getAllIngredients() {
-    return fetch('http://localhost:3001/api/v1/ingredients').then(response => {
-      return response.json();
-    }).catch(error => {
-      return error;
-    });
-  }
-}
