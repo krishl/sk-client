befd4c6c4d38bc16220116286afd1e273b423b75
github username <krishel.lasam@gmail.com>
2017-07-15 04:28:38 -0400

    remove attributes and retrieveIngredients from ProductShow

--- src/components/containers/ProductShow.js
+++ src/components/containers/ProductShow.js
@@ -35,36 +35,27 @@ class ProductShow extends Component {
 
     return (
       <div className={css(sheet.content)}>
-        <h2>{this.props.product.attributes.name}</h2>
-        Brand: {this.props.product.attributes.brand}
+        <h2>{this.props.product.name}</h2>
+        Brand: {this.props.product.brand}
         <IngredientsList ingredients={this.props.ingredients} /> 
       </div>
     );
   }
 };
 
-function retrieveIngredients(ingredients, product) {  
-  let ids = product.relationships.ingredients.data.map(ingId => ingId.id)
-  let retrieved = ingredients.filter(ingredient => {
-    return ids.includes(ingredient.id)
-  })
-  return retrieved
-}
-
 ProductShow.propTypes = {  
   product: PropTypes.object.isRequired,
   ingredients: PropTypes.array.isRequired
 };
 
-function mapStateToProps(state, ownProps) {  
-  let product = {attributes: {}};
+function mapStateToProps(state, ownProps) {
+  let id = parseInt(ownProps.match.params.id, 10)
+  let product = {};
   let ingredients = [];
   if (state.products.length > 0) {
-    product = Object.assign({}, state.products.find(product => product.id === 
-      ownProps.match.params.id)
-    )
-    if (product.id && product.relationships.ingredients.data.length > 0) {
-      ingredients = retrieveIngredients(state.ingredients, product)
+    product = state.products.find(product => product.id === id)
+    if (product.ingredients.length > 0) {
+      ingredients = product.ingredients
     }
   }
   return { product, ingredients }
