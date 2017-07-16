8ec077595bbe99d392d3a0b19955fe187af32367
github username <krishel.lasam@gmail.com>
2017-07-16 06:11:43 -0400

    set up for individual css

--- src/components/IndividualProduct.js
+++ src/components/IndividualProduct.js
@@ -3,23 +3,37 @@ import IngredientsList from './IngredientsList';
 import {StyleSheet, css} from 'aphrodite-jss';
 
 const sheet = StyleSheet.create({
-  content: {
+  products: {
     width: 'auto',
     padding: '5px',
     height: '100%',
-    float: 'right',
     overflowY: 'auto',
-    position: 'relative'
+    position: 'center'
+  },
+  compare: {
+    width: 'auto',
+    padding: '8px',
+    height: 'auto',
+    float: 'right',
+    borderStyle: 'dashed',
+    borderWidth: '1px',
+    borderColor: '#ddd',
+    position: 'relative',
+    alignContent: 'flex-start',
+    flexWrap: 'wrap'
   }
 })
 
 export default class IndividualProduct extends Component {  
   render() {
     return (
-      <div className={css(sheet.product)}>
-        <h2>{this.props.product.name}</h2>
-        Brand: {this.props.product.brand}
-        <IngredientsList ingredients={this.props.product.ingredients} /> 
+      <div>
+        <div className={css(sheet.products)}>
+          <h2>{this.props.product.name}</h2>
+          Brand: {this.props.product.brand}
+          <IngredientsList ingredients={this.props.product.ingredients} /> 
+          <br />
+        </div>
       </div>
     );
   }
