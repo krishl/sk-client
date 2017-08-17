0efda5637d3745129fdaa6bfc76e88871607f1ee
github username <krishel.lasam@gmail.com>
2017-08-17 09:44:00 -0400

    link to products on ProductBrand

--- src/components/ProductBrand.js
+++ src/components/ProductBrand.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import { Link } from 'react-router-dom';
 
 export default class ProductBrand extends Component {  
   render() {
@@ -8,7 +9,7 @@ export default class ProductBrand extends Component {
         <h3>Products</h3>
         <ul>
           {this.props.products.map(product => 
-          <li key={product.id}>{product.name}</li>
+          <li key={product.id}><Link to={`/products/${product.id}`}>{product.name}</Link></li>
           )}
         </ul>
       </div>
